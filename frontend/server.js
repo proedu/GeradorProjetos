339200cde5bf3cf9db61f9f68a045de03e3a6e0b
Raymundo Carlos Machado Ferreira Filho <45569912+pakafe@users.noreply.github.com>
2021-03-28 22:36:42 -0300

    Add files via upload

---
+++ frontend/server.js
@@ -0,0 +1,18 @@
+//Importa as dependências que acabamos de instalar
+const express = require('express');
+//const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/scriba-front'));
+
+app.get('/*', (req,res) => {
+    res.sendFile(__dirname + '/dist/scriba-front/index.html');
+});
+
+// Inicia a aplicação pela porta configurada
+app.listen(PORT, () => {
+    console.log('Servidor iniciador na porta ' + PORT);
+});
